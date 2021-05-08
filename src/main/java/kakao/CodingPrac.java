c124b6f3c5e3b02f6f62be89c75d556f4563d65d
HyoungUk <dnr759@naver.com>
2021-05-08 14:39:42 +0900

    알고리즘 문제 잠깐 품

---
+++ src/main/java/kakao/CodingPrac.java
@@ -0,0 +1,47 @@
+package kakao;
+
+public class CodingPrac {
+
+    public int souu(String hell) {
+        int answer = 0;
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "one2three4zero9seven";
+        s = s.replace("zero", "0");
+        s = s.replace("one","1");
+        s = s.replace("two", "2");
+        s = s.replace("three", "3");
+        s = s.replace("four", "4");
+        s = s.replace("five", "5");
+        s = s.replace("six", "6");
+        s = s.replace("seven", "7");
+        s = s.replace("eight", "8");
+        s = s.replace("nine", "9");
+
+    }
+
+    static class Solution {
+
+        public int solution(String s) {
+            s = s.replace("zero", "0");
+            s = s.replace("one","1");
+            s = s.replace("two", "2");
+            s = s.replace("three", "3");
+            s = s.replace("four", "4");
+            s = s.replace("five", "5");
+            s = s.replace("six", "6");
+            s = s.replace("seven", "7");
+            s = s.replace("eight", "8");
+            s = s.replace("nine", "9");
+
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+
+    }
+
+}
