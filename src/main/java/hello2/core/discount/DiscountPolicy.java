ce4d43cad805b274eeaeed335521cc158a365d5a
HyoungUk <dnr759@naver.com>
2021-05-08 11:02:33 +0900

    패키지 정리!

--- src/main/java/hello2/core/discount/DiscountPolicy.java
+++
@@ -1,9 +0,0 @@
-package hello2.core.discount;
-
-import hello2.core.member.Member;
-
-public interface DiscountPolicy {
-
-    int discount(Member member, int price);
-
-}
--- src/main/java/hello2/core/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package hello2.core.member;
-
-// 저장소
-public interface MemberRepository {
-
-    void save(Member member);
-
-    Member findById(Long memberId);
-}
